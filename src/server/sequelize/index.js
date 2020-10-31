07036354e9655277d3fc44a3e5ddd484ea890d24
junners <jose.junryan@gmail.com>
2020-10-31 11:04:10 +0800

    test heroku postgres connection

--- src/server/sequelize/index.js
+++ src/server/sequelize/index.js
@@ -16,7 +16,6 @@ const { Sequelize } = require('sequelize');
 // console.log(process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: true,
   },
