a0581c6c8c773ae294e390314232fc69980e0f4e
junners <jose.junryan@gmail.com>
2020-10-31 11:07:27 +0800

    rejectunauthorized fix

--- src/server/sequelize/index.js
+++ src/server/sequelize/index.js
@@ -17,7 +17,10 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
 
